4df4754c625071635032a33cb6c5747c44bc5173
y.egorov <y.egorov@city-mobil.ru>
2022-01-05 00:15:08 +0300

    first

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Юрий Егоров on 05.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
